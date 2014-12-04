83a294a7f94ab31e59c0ce0990abb5efa81e48a5
lucybrooks <lucybrooks@users.noreply.github.com>
2014-12-04 07:55:46 -0700

    Update Line.java

--- src/Line.java
+++ src/Line.java
@@ -1,1 +1,39 @@
-// A Line object represents a line defined by a pair of Points.
+public class Line
+{
+  private Point start;
+  private Point end;
+  
+  public Line()
+  {
+    start=new Point();
+    end=new Point();
+  }
+  
+  //constructor
+  public Line(Point startPoint, Point endPoint)
+  {
+    start=startPoint;
+    end=endPoint;
+  }
+  
+  // accessors
+  public double length()
+  {
+    return start.distance(end);
+  }
+  public double slope()
+  {
+    return (start.getY()-end.getY())/(start.getX()-end.getX());
+  }
+  public String toString()
+  {
+    return "["+start.toString()+","+end.toString()+"]";
+  }
+  
+  // mutators
+  public void translate(int dx, int dy)
+  {
+    start.translate(dx,dy);
+    end.translate(dx, dy);
+  }
+}
